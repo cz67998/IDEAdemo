7548364c3b0ee937c2cd6f7d753091e7a2b8951a
cz67998 <413874736@qq.com>
2018-12-27 11:59:33 +0800

    18.12.27

--- src/main/java/com/wangzhou/demo/TestController.java
+++ src/main/java/com/wangzhou/demo/TestController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping("/test")
     public  long test(){
+        System.out.println("test");
         return 200;
     }
 }
